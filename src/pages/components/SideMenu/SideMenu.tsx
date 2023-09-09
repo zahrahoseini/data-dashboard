db02c52b4551792038c90a898899152dd83bc0e3
z.hoseini <z.hoseini@asiatech.ir>
2023-09-09 13:12:58 +0330

    wip: project structure created

---
+++ src/pages/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideMenu = () => {
+  return (
+    <ul>
+      <li>Analitycs</li>
+      <li>Settings</li>
+      <li>Profile</li>
+    </ul>
+  );
+};
+
+export default SideMenu;
